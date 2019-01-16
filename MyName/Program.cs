a6420a1a12712eb070607b36b55f856e311ff523
subhanisyed17@gmail.com <subhanisyed17@gmail.com>
2019-01-16 06:16:32 +0530

    Add project files.

---
+++ MyName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyName
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "subhani syed";
+			Debug.WriteLine(name);
+		}
+	}
+}
